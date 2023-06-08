2078860466e9699e9d388019cf1ef15d33150647
dobe0715 <121845677+dobe0715@users.noreply.github.com>
2023-06-08 10:05:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+
+
+
+# In[1]:
+
+
+import easydict
+# import argparse
+import traceback
+import shutil
+import logging
+import yaml
+import sys
+import os
+import torch
+import numpy as np
+# import torch.utils.tensorboard as tb
+import copy
+
+from runner import Diffusion
+
+
+# In[2]:
+
+
+def make_parse_args():
+    args = easydict.EasyDict({'seed': 1234, 
+                              'exp': 'exp', 
+                              'comment': '', 
+                              'verbose': 'info', 
+                              'sample': 'store_true', 
+                              'i': 'images', 
+                              'image_folder': 'images', 
+                              'ni': 'store_true', 
+                              'sample_step': 3, 
+                              't': 400})
+
+    level = getattr(logging, args.verbose.upper(), None)
+    if not isinstance(level, int):
+        raise ValueError('level {} not supported'.format(args.verbose))
+
+    handler1 = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
+    handler1.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler1)
+    logger.setLevel(level)
+
+    os.makedirs(os.path.join(args.exp, 'image_samples'), exist_ok=True)
+    args.image_folder = os.path.join(args.exp, 'image_samples', args.image_folder)
+    if not os.path.exists(args.image_folder):
+        os.makedirs(args.image_folder)
+    else:
+        overwrite = False
+        if args.ni:
+            overwrite = True
+        else:
+            response = input("Image folder already exists. Overwrite? (Y/N)")
+            if response.upper() == 'Y':
+                overwrite = True
+
+        if overwrite:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        else:
+            print("Output image folder exists. Program halted.")
+            sys.exit(0)
+
+    # add device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    logging.info("Using device: {}".format(device))
+
+    # set random seed
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+
+    torch.backends.cudnn.benchmark = True
+
+    return args
+
+
+# In[3]:
+
+
+args = make_parse_args()
+config = "celeba.yml"
+origin_img = input("원본 이미지 경로 입력해주세요. : ")
+stroked_img = input("수정한 이미지 경로 입력해주세요. : ")
+
+try:
+    runner = Diffusion(args, config, origin_img, stroked_img)
+    runner.image_editing_sample()
+except Exception:
+    logging.error(traceback.format_exc())
+
---
+++ runner.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import yaml
+import os
+import numpy as np
+from tqdm import tqdm
+from PIL import Image
+
+import torch
+import torchvision
+import torchvision.utils as tvu
+
+from models.diffusion import Model
+from utils.diffusion_util import get_beta_schedule, extract, image_editing_denoising_step_flexible_mask
+from utils.image_process import preprocess
+
+
+import argparse
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
+
+
+# In[18]:
+
+
+class Diffusion:
+    def __init__(self, args, config, origin_img, stroked_img, device=None):
+        self.args = args
+        self.config = config
+        self.origin_img = origin_img
+        self.stroked_img = stroked_img
+
+        with open(self.config, 'r') as f:
+            config = yaml.safe_load(f)
+            config = dict2namespace(config)
+
+        self.config = config
+
+        if device is None:
+            device = torch.device(
+                "cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = device
+
+        self.model_var_type = config.model.var_type
+        betas = get_beta_schedule(
+            beta_start=config.diffusion.beta_start,
+            beta_end=config.diffusion.beta_end,
+            num_diffusion_timesteps=config.diffusion.num_diffusion_timesteps
+        )
+        self.betas = torch.from_numpy(betas).float().to(self.device)
+        self.num_timesteps = betas.shape[0]
+
+        alphas = 1.0 - betas
+        alphas_cumprod = np.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = np.append(1.0, alphas_cumprod[:-1])
+        posterior_variance = betas *             (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
+        if self.model_var_type == "fixedlarge":
+            self.logvar = np.log(np.append(posterior_variance[1], betas[1:]))
+
+        elif self.model_var_type == 'fixedsmall':
+            self.logvar = np.log(np.maximum(posterior_variance, 1e-20))
+
+    def image_editing_sample(self):
+        print("Loading model")
+
+        url = "./models/celeba_hq.ckpt"
+        ckpt = torch.load(url, map_location=self.device)
+        config = "celeba.yml"
+        
+        model = Model(self.config)
+        model.load_state_dict(ckpt)
+        model.to(self.device)
+        model = torch.nn.DataParallel(model)
+        print("Model loaded")
+        ckpt_id = 0
+        
+        
+        # n = self.config.sampling.batch_size
+        n = 4
+        model.eval()
+        print("Start sampling")
+        with torch.no_grad():
+            origin_img, stroked_img = self.origin_img, self.stroked_img
+
+            resolution = self.config.data.image_size
+            
+            img, mask = preprocess(origin_img, stroked_img, resolution=resolution)
+            
+            mask = mask.to(self.device)
+            img = img.to(self.device)
+
+            img = img.unsqueeze(dim=0)
+            img = img.repeat(n, 1, 1, 1)
+            
+            x0 = img            
+
+            tvu.save_image(x0, os.path.join(self.args.image_folder, f'original_input.png'))
+            x0 = (x0 - 0.5) * 2.
+
+            for it in range(self.args.sample_step):
+                e = torch.randn_like(x0)
+                total_noise_levels = self.args.t
+                a = (1 - self.betas).cumprod(dim=0)
+                x = x0 * a[total_noise_levels - 1].sqrt() + e * (1.0 - a[total_noise_levels - 1]).sqrt()
+                tvu.save_image((x + 1) * 0.5, os.path.join(self.args.image_folder, f'init_{ckpt_id}.png'))
+
+                with tqdm(total=total_noise_levels, desc="Iteration {}".format(it)) as progress_bar:
+                    for i in reversed(range(total_noise_levels)):
+                        t = (torch.ones(n) * i).to(self.device)
+                        x_ = image_editing_denoising_step_flexible_mask(x, t=t, model=model,
+                                                                        logvar=self.logvar,
+                                                                        betas=self.betas)
+                        x = x0 * a[i].sqrt() + e * (1.0 - a[i]).sqrt()
+                        x[:, (mask != 1.)] = x_[:, (mask != 1.)]
+                        # added intermediate step vis
+                        if (i - 99) % 100 == 0:
+                            tvu.save_image((x + 1) * 0.5, os.path.join(self.args.image_folder,
+                                                                       f'noise_t_{i}_{it}.png'))
+                        progress_bar.update(1)
+
+                x0[:, (mask != 1.)] = x[:, (mask != 1.)]
+                torch.save(x, os.path.join(self.args.image_folder,
+                                           f'samples_{it}.pth'))
+                tvu.save_image((x + 1) * 0.5, os.path.join(self.args.image_folder,
+                                                           f'samples_{it}.png'))
